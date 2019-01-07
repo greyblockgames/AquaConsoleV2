3059c224fb0d8e0263c4b5534a496ff71ca06be9
Lukas Olson <lukasdragon@live.ca>
2019-01-06 16:43:24 -0800

    Documentation for ChangeTitle

--- AquaConsoleV2/AquaConsoleV2/console.cpp
+++ AquaConsoleV2/AquaConsoleV2/console.cpp
@@ -13,6 +13,7 @@ namespace ac_console
 #ifdef WIN32 
 		return SetConsoleTitle(title.c_str());
 #elif __APPLE__
+		//todo
 		return false;
 #elif __linux__
 		std::cout << "\033]0;" << title << "\007";
--- AquaConsoleV2/AquaConsoleV2/console.h
+++ AquaConsoleV2/AquaConsoleV2/console.h
@@ -1,6 +1,11 @@
 #pragma once
 
 namespace ac_console {
+	/**
+	 * \brief Changes the console title.
+	 * \param title String to change title to
+	 * \return Bool stating whether the change succeeded. Note: Returns false only on windows.
+	 */
 	bool new_title(std::string title);
 
 }
