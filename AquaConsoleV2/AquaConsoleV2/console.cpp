3495effcecb06ae3477c3bd95572321eda5068e9
Lukas Olson <lukasdragon@live.ca>
2019-01-06 16:25:58 -0800

    Application now changes console title when running!

---
+++ AquaConsoleV2/AquaConsoleV2/console.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "console.h"
+
+
+#ifdef WIN32
+#include <windows.h>
+#endif
+
+namespace ac_console
+{
+	bool new_title(std::string title)
+	{
+#ifdef WIN32 
+		return SetConsoleTitle(title.c_str());
+#elif __APPLE__
+		return false;
+#elif __linux__
+		std::cout << "\033]0;" << title << "\007";
+		return true;
+#endif
+	}
+}
---
+++ AquaConsoleV2/AquaConsoleV2/console.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace ac_console {
+	bool new_title(std::string title);
+
+}
